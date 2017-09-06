672bf32b561ce96007a57572f52dab98cb5a5400
kingcos <2821836721v@gmail.com>
2017-09-06 15:44:19 +0800

    Add options

--- WWDC_CLI/Package.swift
+++ WWDC_CLI/Package.swift
@@ -3,5 +3,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "WWDC_CLI"
+    name: "WWDC_CLI",
+    dependencies: [
+        .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1"),
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2)
+    ]
 )
--- WWDC_CLI/Sources/main.swift
+++ WWDC_CLI/Sources/main.swift
@@ -1,1 +1,62 @@
-print("Hello, world!")
+import Foundation
+import CommandLineKit
+import Rainbow
+
+let cli = CommandLineKit.CommandLine()
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.blue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s: str, type: type)
+}
+
+let yearOption = IntOption(shortFlag: "y", longFlag: "year",
+                           helpMessage: "Year of WWDC. Default is WWDC 2017.")
+cli.addOption(yearOption)
+
+let listOption = BoolOption(shortFlag: "l", longFlag: "list",
+                            helpMessage: "List all sessions info.")
+cli.addOption(listOption)
+
+let sessionOption = MultiStringOption(shortFlag: "s", longFlag: "session",
+                                      helpMessage: "Session number in WWDC.")
+cli.addOption(sessionOption)
+
+let languageOption = StringOption(shortFlag: "g", longFlag: "language",
+                                  helpMessage: "Subtitle in which language. Default is Chinese.")
+cli.addOption(languageOption)
+
+let helpOption = BoolOption(shortFlag: "h", longFlag: "help",
+                            helpMessage: "Print this help info.")
+cli.addOption(helpOption)
+
+let versionOption = BoolOption(shortFlag: "v", longFlag: "version",
+                               helpMessage: "Print version info.")
+cli.addOption(versionOption)
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if helpOption.value {
+    cli.printUsage()
+    exit(EX_OK)
+}
+
+let year = yearOption.value
+let isList = listOption.value
+let sessions = sessionOption.value
+let language = languageOption.value
