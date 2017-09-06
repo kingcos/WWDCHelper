694cb9f86f270f4b6ed6d1787ebeb56bc56c699e
kingcos <2821836721v@gmail.com>
2017-09-07 03:01:22 +0800

    Extend String

---
+++ WWDC_CLI/Sources/WWDC_CLI_Kit/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  WWDC_CLI
+//
+//  Created by kingcos on 07/09/2017.
+//
+//
+
+import Foundation
+
+extension String {
+    var wholeNSRange: NSRange {
+        return NSRange(location: 0, length: characters.count)
+    }
+    
+    func substring(with range: NSRange) {
+        let nsRange = NSRange(location: range.location, length: range.length)
+        substring(with: nsRange)
+    }
+}
