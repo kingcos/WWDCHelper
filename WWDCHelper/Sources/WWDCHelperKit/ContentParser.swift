8863b98c45629f271ec2bfb323f5e949d5046340
kingcos <2821836721v@gmail.com>
2017-09-08 03:01:45 +0800

    Refactor ContentParser

--- WWDCHelper/Sources/WWDCHelperKit/ContentParser.swift
+++ WWDCHelper/Sources/WWDCHelperKit/ContentParser.swift
@@ -8,17 +8,53 @@
 
 import Foundation
 
-protocol ContentParsable {
-    func parse(in content: String) -> [String : String]
+enum SessionInfoType {
+    case subtitleIndexURLPrefix
+    case resources
+    case sessionsInfo
 }
 
-protocol RegexContentParsable: ContentParsable {
-    var pattern: String { get }
+protocol SessionInfoParsable {
+    func parseSubtitleIndexURLPrefix(in content: String) -> String
+    func parseResources(in content: String) -> [String]
+    func parseSessionsInfo(in content: String) -> [String : String]
 }
 
-extension RegexContentParsable {
-    func parse(in content: String) -> [String : String] {
-        let regex = try! NSRegularExpression(pattern: pattern)
+protocol RegexSessionInfoParsable: SessionInfoParsable {
+    var patterns: [SessionInfoType : String] { get }
+}
+
+extension RegexSessionInfoParsable {
+    func parseSubtitleIndexURLPrefix(in content: String) -> String {
+        let regex = try! NSRegularExpression(pattern: patterns[.subtitleIndexURLPrefix]!)
+        let range = content.wholeNSRange
+        let matches = regex.matches(in: content, range: range)
+        
+        var result = ""
+        for match in matches {
+            let firstRange = match.rangeAt(1)
+            result = content.substring(with: firstRange)
+        }
+        
+        return result
+    }
+    
+    func parseResources(in content: String) -> [String] {
+        let regex = try! NSRegularExpression(pattern: patterns[.resources]!)
+        let range = content.wholeNSRange
+        let matches = regex.matches(in: content, range: range)
+        
+        var result = [String]()
+        for match in matches {
+            let firstRange = match.rangeAt(1)
+            result.append(content.substring(with: firstRange))
+        }
+        
+        return result
+    }
+    
+    func parseSessionsInfo(in content: String) -> [String : String] {
+        let regex = try! NSRegularExpression(pattern: patterns[.sessionsInfo]!)
         let range = content.wholeNSRange
         let matches = regex.matches(in: content, range: range)
         
@@ -33,6 +69,10 @@ extension RegexContentParsable {
     }
 }
 
-struct SessionParser: RegexContentParsable {
-    var pattern = "<a href=\"\\/videos\\/play\\/wwdc[0-9]{4}\\/([0-9]*)\\/\".*\\n.*<h4.*>(.*)</h4>"
+struct SessionParser: RegexSessionInfoParsable {
+    let patterns: [SessionInfoType : String] = [
+        .subtitleIndexURLPrefix: "(http.*)\\/.*_hd",
+        .resources: "<ul class=\"links small\">[\\s\\S]*<a href=\"(https.*dl=1)\">[\\s\\S]*<a href=\"(https.*dl=1)\">[\\s\\S]*<a href=\"(https.*pdf)\">",
+        .sessionsInfo: "<a href=\"\\/videos\\/play\\/wwdc[0-9]{4}\\/([0-9]*)\\/\".*\\n.*<h4.*>(.*)</h4>"
+    ]
 }
