9dfa0a52d6ac9272c235b32ebb7adf96d87663cd
kingcos <2821836721v@gmail.com>
2018-08-22 00:43:43 +0800

    Update Rainbow

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "WWDCHelper",
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.1"),
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.1"),
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.4"),
         .package(url: "https://github.com/kylef/Spectre.git", from: "0.8.0"),
         .package(url: "https://github.com/kingcos/CommandLine.git", from: "4.1.0")
     ],
