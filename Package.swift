3666926015611d23c86ae53fc4d3fbe1d8d50f83
kingcos <2821836721v@gmail.com>
2018-10-18 18:24:39 +0800

    Support Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WWDCHelper",
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.1"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.2"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.4"),
-        .package(url: "https://github.com/kylef/Spectre.git", from: "0.8.0"),
-        .package(url: "https://github.com/kingcos/CommandLine.git", from: "4.1.0")
+        .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
+        .package(url: "https://github.com/kingcos/CommandLine.git", from: "4.2.0")
     ],
     targets: [
         .target(
