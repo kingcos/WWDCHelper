b3a59ebbebb8acfa48b5f114da13f4d9f4173440
kingcos <2821836721v@gmail.com>
2019-08-20 00:37:33 +0800

    Update to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "WWDCHelper",
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.2"),
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.4"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.5"),
         .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0"),
         .package(url: "https://github.com/kingcos/CommandLine.git", from: "4.2.0")
     ],
